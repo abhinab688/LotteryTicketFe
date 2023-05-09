1c44157ff27990b3a07e15fd8f482292ff21174e
Abhinab Saikia <109782817+abhinab688@users.noreply.github.com>
2023-05-09 20:25:55 +0530

    Add files via upload

---
+++ Js/home.js
@@ -0,0 +1,6 @@
+// const buyBtn = document.getElementById('buy-btn-1');
+// console.log(buyBtn)
+
+// buyBtn.addEventListener('click', () => {
+//     window.location.href = './SelectTicNum.html'
+// })
